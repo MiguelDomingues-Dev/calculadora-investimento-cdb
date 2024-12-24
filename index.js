8e028838be7110d5a3f1834e35eb28646fe85b94
Miguel-PC1317 <85039271+MiguelDomingues-Dev@users.noreply.github.com>
2024-12-24 18:18:16 -0300

    Add modificações

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const resultado = document.querySelector("#resultado");
 const porcentagem = document.querySelector("#porcentagem");
 
+const mesesTotal = document.querySelector("#meses");
+const ganho = document.querySelector("#realInvest");
+
+const ganhoSemAporteTotal = document.querySelector("#totalGanhoSemAporte");
+const ganhoComAporteTotal = document.querySelector("#totalGanhoComAporte");
+
 document.querySelector("#calcular").addEventListener('click', function () {
     const redimento = parseFloat(document.querySelector("#rendN").value);
     const cdi = parseFloat(document.querySelector("#taxaCdiHj").value);
@@ -9,16 +15,31 @@ document.querySelector("#calcular").addEventListener('click', function () {
     const meses = parseInt(document.querySelector("#quantMensal").value);
     console.log(meses);
 
+    //Transformando % em Decimal
     const cdiDecimal = cdi / 100;
     const redimentoDecimal = redimento / 100;
-    
+
     //Taxa anual ajustada para % do CDI
     const taxaAnual = cdiDecimal * redimentoDecimal;
     const taxaMensal = (1 + taxaAnual) ** (1 / 12) - 1;
 
+    const montanteSemPorte = porteMensal * (1 + taxaMensal) ** meses;
+
+    const totalSemAporte = montanteSemPorte - porteMensal;
+
+
     //Calculando o montante
     const montanteCdi = porteMensal * (((1 + taxaMensal) ** meses - 1) / taxaMensal) * (1 + taxaMensal);
-    console.log(montanteCdi);
+    
+    const totalComAporte = montanteCdi - porteMensal;
+
+    //Resultado
     porcentagem.innerHTML = redimento;
     resultado.innerHTML = montanteCdi.toFixed(2);
+
+    mesesTotal.innerHTML = meses;
+    ganho.innerHTML = montanteSemPorte.toFixed(2);
+
+    ganhoSemAporteTotal.innerHTML = totalSemAporte.toFixed(2);
+    ganhoComAporteTotal.innerHTML = totalComAporte.toFixed(2);
 })
